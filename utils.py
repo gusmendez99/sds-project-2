3cfe388445f9be5482098cd9e2f2e12ce057204b
gusmendez99 <gus.mendez.99@gmail.com>
2022-04-20 12:50:22 -0600

    [ADD] eda & preprocessing

--- utils.py
+++ utils.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 def reduce_memory_usage(df, verbose=True):
-    numerics = ["int8", "int16", "int32", "int64", "float16", "float32", "float64"]
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
     start_mem = df.memory_usage().sum() / 1024 ** 2
     for col in df.columns:
         col_type = df[col].dtypes
         if col_type in numerics:
             c_min = df[col].min()
             c_max = df[col].max()
-            if str(col_type)[:3] == "int":
+            if str(col_type)[:3] == 'int':
                 if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
                     df[col] = df[col].astype(np.int8)
                 elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
@@ -16,20 +16,15 @@ def reduce_memory_usage(df, verbose=True):
                 elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
                     df[col] = df[col].astype(np.int32)
                 elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
-                    df[col] = df[col].astype(np.int64)
+                    df[col] = df[col].astype(np.int64)  
             else:
-                if (
-                    c_min > np.finfo(np.float16).min
-                    and c_max < np.finfo(np.float16).max
-                ):
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
                     df[col] = df[col].astype(np.float16)
-                elif (
-                    c_min > np.finfo(np.float32).min
-                    and c_max < np.finfo(np.float32).max
-                ):
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
                     df[col] = df[col].astype(np.float32)
                 else:
-                    df[col] = df[col].astype(np.float64)
+                    df[col] = df[col].astype(np.float64)    
+            
     end_mem = df.memory_usage().sum() / 1024 ** 2
     if verbose:
         print(
@@ -54,3 +49,23 @@ def get_dataset_dtypes():
     categorical_columns = [c for c,v in dtypes.items() if v not in numerics]
 
     return dtypes, numerical_columns, categorical_columns
+
+
+def derivate_features(df):
+    df['EngineVersion_2'] = df['EngineVersion'].apply(lambda x: x.split('.')[2]).astype('category')
+    df['EngineVersion_3'] = df['EngineVersion'].apply(lambda x: x.split('.')[3]).astype('category')
+
+    df['AppVersion_1'] = df['AppVersion'].apply(lambda x: x.split('.')[1]).astype('category')
+    df['AppVersion_2'] = df['AppVersion'].apply(lambda x: x.split('.')[2]).astype('category')
+    df['AppVersion_3'] = df['AppVersion'].apply(lambda x: x.split('.')[3]).astype('category')
+
+    df['Census_OSVersion_0'] = df['Census_OSVersion'].apply(lambda x: x.split('.')[0]).astype('category')
+    df['Census_OSVersion_1'] = df['Census_OSVersion'].apply(lambda x: x.split('.')[1]).astype('category')
+    df['Census_OSVersion_2'] = df['Census_OSVersion'].apply(lambda x: x.split('.')[2]).astype('category')
+    df['Census_OSVersion_3'] = df['Census_OSVersion'].apply(lambda x: x.split('.')[3]).astype('category')
+
+    df['PrimaryDriveRatio'] = df['Census_SystemVolumeTotalCapacity']/ df['Census_PrimaryDiskTotalCapacity']
+    df['NonPrimaryDriveMB'] = df['Census_PrimaryDiskTotalCapacity'] - df['Census_SystemVolumeTotalCapacity']
+    df['RAM_Per_Processor'] = df['Census_TotalPhysicalRAM']/ df['Census_ProcessorCoreCount']
+    
+    return df
