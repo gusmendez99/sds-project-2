389e62fda3658bd0bc23fea5eaa8b31bb9134b6e
michelebenveuto <35434145+michelebenvenuto@users.noreply.github.com>
2022-04-21 20:02:05 -0600

    [ADD] Naive Bayes Model

--- utils.py
+++ utils.py
@@ -68,87 +68,28 @@ def derivate_features(df):
 
 
 def get_roc_curves(y, y_pred):
-    y_ = label_binarize(y, classes=[0, 1, 2, 3])
-    pred = label_binarize(y_pred, classes=[0,1,2,3])
-    
+    y_ = y
+    pred = y_pred
     # Compute ROC curve and ROC area for each class
     fpr = dict()
     tpr = dict()
     roc_auc = dict()
-    n_classes=4
+    n_classes=1
     for i in range(n_classes):
-        fpr[i], tpr[i], _ = roc_curve(y_[:, i], pred[:, i])
-        roc_auc[i] = auc(fpr[i], tpr[i])
-
-    # Compute micro-average ROC curve and ROC area
-    fpr["micro"], tpr["micro"], _ = roc_curve(y_.ravel(), pred.ravel())
-    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+        fpr, tpr, _= roc_curve(y, y_pred)
+        roc_auc = auc(fpr, tpr)
 
     plt.figure()
     lw = 2
     plt.plot(
-        fpr[2],
-        tpr[2],
+        fpr,
+        tpr,
         color="darkorange",
         lw=lw,
-        label="ROC curve (area = %0.2f)" % roc_auc[2],
+        label="ROC curve (area = %0.2f)" % roc_auc,
     )
     plt.rcParams["figure.figsize"] = (5,5)
     plt.plot([0, 1], [0, 1], color="navy", lw=lw, linestyle="--")
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel("False Positive Rate")
-    plt.ylabel("True Positive Rate")
-    plt.title("Curva ROC")
-    plt.legend(loc="lower right")
-    plt.show()
-    
-    # First aggregate all false positive rates
-    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
-
-    # Then interpolate all ROC curves at this points
-    mean_tpr = np.zeros_like(all_fpr)
-    for i in range(n_classes):
-        mean_tpr += np.interp(all_fpr, fpr[i], tpr[i])
-
-    # Finally average it and compute AUC
-    mean_tpr /= n_classes
-
-    fpr["macro"] = all_fpr
-    tpr["macro"] = mean_tpr
-    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
-
-    # Plot all ROC curves
-    plt.figure()
-    plt.plot(
-        fpr["micro"],
-        tpr["micro"],
-        label="micro-average ROC curve (area = {0:0.2f})".format(roc_auc["micro"]),
-        color="deeppink",
-        linestyle=":",
-        linewidth=4,
-    )
-
-    plt.plot(
-        fpr["macro"],
-        tpr["macro"],
-        label="macro-average ROC curve (area = {0:0.2f})".format(roc_auc["macro"]),
-        color="navy",
-        linestyle=":",
-        linewidth=4,
-    )
-
-    colors = cycle(["aqua", "darkorange", "cornflowerblue"])
-    for i, color in zip(range(n_classes), colors):
-        plt.plot(
-            fpr[i],
-            tpr[i],
-            color=color,
-            lw=lw,
-            label="ROC curve of class {0} (area = {1:0.2f})".format(i, roc_auc[i]),
-        )
-
-    plt.plot([0, 1], [0, 1], "k--", lw=lw)
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
     plt.xlabel("False Positive Rate")
